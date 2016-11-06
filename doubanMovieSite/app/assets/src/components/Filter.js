a5bd2f2411b7ceded4c830d675e5d890130b9193
zq <c5e1856@gmail.com>
2016-11-07 00:05:07 +0800

    add eslint

--- doubanMovieSite/app/assets/src/components/Filter.js
+++ doubanMovieSite/app/assets/src/components/Filter.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 export class Filter extends Component {
+
+    constructor() {
+        super();
+    }
+
+    static defaultProps = {
+        year: [1980, 2020],
+        genres: [],
+
+    }
+
     render() {
         return (
             <div>
